8abfe55c3596e137d8d9d4f3465dc3b0a5989f27
paulahn <jyahn0623@gmail.com>
2022-07-18 13:28:30 +0900

    fc

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
+import { BackHandler, StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants'
+import { useEffect, useRef } from 'react';
 
 export default function App() {
+  const WebviewRef = useRef();
+
+  useEffect(() => {
+    const backHandler = () => {
+      WebviewRef.current.goBack();
+      return true;
+    }
+    BackHandler.addEventListener("hardwareBackPress", backHandler);
+
+    return () => BackHandler.removeEventListener("hardwareBackPress", backHandler);
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex : 1, paddingTop : Constants.statusBarHeight}}> 
+      <StatusBar 
+        style="dark" 
+        backgroundColor="#fff"
+      />
+      <WebView 
+        ref={WebviewRef}
+        style={styles.container}
+        source={{ uri: 'http://58.120.166.106:7000/' }}
+      />
     </View>
   );
 }
